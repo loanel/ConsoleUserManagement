6262c8fc90a8478c47d06f2d30c6e3994a51595b
loanel <machowski.mat@gmail.com>
2018-05-27 01:41:25 +0200

    initial commmit, project setup with spring boot

---
+++ src/main/java/com/data/test/ConsoleUserManagement/ConsoleUserManagementApplication.java
@@ -0,0 +1,14 @@
+package com.data.test.ConsoleUserManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleUserManagementApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ConsoleUserManagementApplication.class, args);
+		
+	}
+}
