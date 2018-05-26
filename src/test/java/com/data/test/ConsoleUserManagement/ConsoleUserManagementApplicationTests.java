ef5c7dbd9ee720bf5514d5d49fb1c9761c3462be
loanel <machowski.mat@gmail.com>
2018-05-27 01:39:57 +0200

    initial commmit, project setup with spring boot

---
+++ src/test/java/com/data/test/ConsoleUserManagement/ConsoleUserManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.data.test.ConsoleUserManagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsoleUserManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
